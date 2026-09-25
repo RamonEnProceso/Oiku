e2d677903c6ca256fb011f8b35ef0983a5e93eab
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-25 12:15:47 -0300

    feat: Añadir modelo Sqlalchemy de Category

---
+++ 01-backend/app/models/category.py
@@ -0,0 +1,22 @@
+from app.db.base import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy import Boolean
+from sqlalchemy import String
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .subcategory import Subcategory
+
+class Category(Base):
+    __tablename__="bills_category"
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    name : Mapped[str] = mapped_column(String)
+    
+    subcategories : Mapped[list["Subcategory"]] = relationship("Subcategory", back_populates="bills_subcategory")
+    
+    def __repr__(self):
+        return f"Category(id={self.id}, name='{self.name}')"
