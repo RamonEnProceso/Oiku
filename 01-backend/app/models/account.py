eb78673f87b5bb21a8044a0622f6ba3d9f7df425
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-25 12:19:57 -0300

    feat: Añadir modelo Sqlalchemy de Account

---
+++ 01-backend/app/models/account.py
@@ -0,0 +1,20 @@
+from app.db.base import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import String
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .bills import Bills
+
+class Account(Base):
+    __tablename__="bills_account"
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    name : Mapped[str] = mapped_column(String)
+    
+    bills : Mapped[list["Bills"]] = relationship("Bills", back_populates="bills_account")
+    
+    def __repr__(self):
+        return f"Account(id={self.id}, name='{self.name}')"
