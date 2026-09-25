3ec7e0e8a6f16bf83a804ca3fbc25c0062cc0819
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-25 15:55:32 -0300

    feat: Añadir ruta que devuelva todos los `bills`

--- 01-backend/app/routers/bills.py
+++ 01-backend/app/routers/bills.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter, Depends
+from app.db.dependencies import get_db
+from app.schemas.bills import BillResponse
+from sqlalchemy.orm import Session
+from app.crud.bills import get_bills
 
 router = APIRouter(
     prefix="/bills",
     tags=["Bills"]
 )
 
+@router.get("/",response_model=list[BillResponse])
+def get_bills_route(db:Session = Depends(get_db)):
+    return get_bills(db)
