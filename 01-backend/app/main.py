b2491d2b2b48a9cd3d10962ca7f6178899b3f08d
RamonEnProceso <ramon.en.proceso@gmail.com>
2026-09-25 11:01:18 -0300

    feat: Añadir router de gastos

--- 01-backend/app/main.py
+++ 01-backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers.gastos import router as router_gastos
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status":"online"}
+
+app.include_router(router_gastos)
---
+++ 01-backend/app/routers/gastos.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter, Depends
+
+router = APIRouter(
+    prefix="/gastos",
+    tags=["Gastos"]
+)
+
